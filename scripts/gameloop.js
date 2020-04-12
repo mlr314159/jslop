bcfb4bd4793d290b32215cbc1e59b2c63b2f786f
Mike <mlr314159@yahoo.co.uk>
2020-04-12 22:31:47 +0000

    card deck sorted  - 1

--- scripts/gameloop.js
+++ scripts/gameloop.js
@@ -1,4 +1,164 @@
 // main game loop for LOP
+var carddeck = [];
+var sorteddeck = [];
+var numplayers = 4;
+var tokensperplayer = 60;
+var dealer = 2;
+var playernames  = ["Me", "Jack", "Fred", "Joe", "Steve", "Willie"];
+
+//player object
+//players array
+
+var randNumArr=[24, 40, 45, 51, 16, 4, 16, 47, 37, 50, 32, 26, 29, 36, 2, 21, 7, 3, 1, 23, 4, 32, 42, 4, 25, 46, 50, 2, 31, 45, 34, 42, 22, 5, 46, 50, 10, 5, 51, 18, 49, 9, 51, 4, 5, 34, 4, 40, 50, 34, 31, 11, 13, 4, 41, 19, 17, 48, 35, 21, 47, 14, 14, 39, 30, 8, 4, 4, 25, 13, 49, 18, 15, 27, 44, 33, 45, 18, 31, 47, 45, 14, 35, 6, 28, 45, 34, 12, 9, 33, 33, 23, 48, 20, 4, 23, 4, 25, 14, 4, 32, 33, 29, 34, 1, 0, 1, 6, 20, 44, 33, 34, 36, 41, 2, 8, 1, 47, 15, 41, 4 ];
+var randNumCount = -1;
+var randRange = -1;
+
+function getRndInteger(x) {
+    if (x <= 0) {
+        if (randNumCount >= 120) {
+            randNumCount = -1;
+        }
+        randNumCount += 1;
+        return randNumArr[randNumCount];
+    }
+    var min = 0;
+    var max = 51;
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+function makecards() {
+    if (carddeck.length > 0) {return "new deck";}
+    console.log("makecards()")
+    var s = "";
+    for (var i = 0; i < 4; i++) {
+        s = "";
+        switch (i) {
+            case 0:
+                s = "s";
+                break;
+            case 1:
+                s = "h";
+                break;
+            case 2:
+                s = "c";
+                break;
+            case 3:
+                s = "d";
+                break;
+        }
+        for (var j = 0; j < 13; j++) {
+            var s1 = ""
+            switch (j) {
+                case 0:
+                    s1 = s + "A";
+                    break;
+                case 1:
+                    s1 = s + "2";
+                    break;
+                case 2:
+                    s1 = s + "3";
+                    break;
+                case 3:
+                    s1 = s + "4";
+                    break;
+                case 4:
+                    s1 = s + "5";
+                    break;
+                case 5:
+                    s1 = s + "6";
+                    break;
+                case 6:
+                    s1 = s + "7";
+                    break;
+                case 7:
+                    s1 = s + "8";
+                    break;
+                case 8:
+                    s1 = s + "9";
+                    break;
+                case 9:
+                    s1 = s + "10";
+                    break;
+                case 10:
+                    s1 = s + "J";
+                    break;
+                case 11:
+                    s1 = s + "Q";
+                    break;
+                case 12:
+                    s1 = s + "K";
+                    break;
+            }
+            carddeck.push(s1);
+        }
+    }
+    console.log ("nd: ",carddeck);
+    console.log ("new deck");
+    return "new deck";
+}
+
+
+function sortcards() {
+    if (sorteddeck.length > 0) {return "cards sorted";}
+    console.log("sortcards()")
+    for (var k = 0; k< 52; k++){
+        var temp = carddeck[k];
+        sorteddeck.push(temp);
+    }
+    var numarray=[];
+    var i = getRndInteger(randRange);
+    var j = 0;
+
+    var tempcardf = "";
+    var tempcardt = "";
+    tempcardf = sorteddeck[0];
+    tempcardt = sorteddeck[i];
+    sorteddeck[i] = tempcardf;
+    sorteddeck[0] = tempcardt;
+
+    i = getRndInteger(randRange);
+    tempcardf = sorteddeck[51];
+    tempcardt = sorteddeck[i];
+    sorteddeck[i] = tempcardf;
+    sorteddeck[51] = tempcardt;
+    
+    for (c = 0; c < 400; c++) {
+        i = getRndInteger(randRange);
+        j = getRndInteger(randRange);
+
+        tempcardf = sorteddeck[j];
+        tempcardt = sorteddeck[i];
+        
+        sorteddeck[i] = tempcardf;
+        sorteddeck[j] = tempcardt;
+    }
+    console.log("sd: ",sorteddeck);
+    console.log ("cards sorted");
+    return "cards sorted";
+}
+
+function showcard(num) {
+    console.log(carddeck[num]);
+    return carddeck[num];
+}
+
+
+function showsortedcard(num) {
+    console.log(sorteddeck[num]);
+    return sorteddeck[num];
+}
+
+function carddescknotsame(deck1,deck2){
+    console.log("carddescknotsame");
+   if(deck1 == deck2){
+       return true;
+   } else {
+       return false;
+   }
+    
+}
+
 
 function gameloop(number_players, initial_coins) {
     console.log("Game Started");
@@ -8,14 +168,18 @@ function gameloop(number_players, initial_coins) {
         console.log(player);
     }
     console.log("Game Ended");
-    if (number_players === -1 && initial_coins === -1 ) {
+    if (number_players === -1 && initial_coins === -1) {
         return " -1 : -1 ";
     }
     return "Game Ended";
 }
 
 //run gameloop
-var players = 2; 
+var players = 2;
 var player_coins = 9;
+var mc = makecards();
+var sc = sortcards();
+console.log(mc,sc);
+console.log(carddescknotsame(carddeck,sorteddeck));
 
-gameloop(players,player_coins);
+var gl = gameloop(players, player_coins);
--- spec/gameloopspec.js
+++ spec/gameloopspec.js
@@ -1,16 +1,107 @@
-describe("gameloop", function() {
-   
-    describe("gameloop", function(){
-        it("should return 'Game Ended'", function() {
+describe("gameloop", function () {
+
+    describe("gameloop", function () {
+        it("should return 'Game Ended'", function () {
             expect(gameloop(3, 100)).toBe('Game Ended');
         });
-    }); 
+    });
 
-    describe("gameloop", function(){
-        it("should return ' -1 : -1 '", function() {
+    describe("gameloop", function () {
+        it("should return ' -1 : -1 '", function () {
             expect(gameloop(-1, -1)).toBe(" -1 : -1 ");
         });
-    }); 
+    });
+
+
+    describe("makecards()", function () {
+        it("should return 'new deck'", function () {
+            expect(makecards()).toBe("new deck");
+        });
+    });
+
+
+    describe("sortcards()", function () {
+        it("should return 'cards sorted'", function () {
+            expect(sortcards()).toBe("cards sorted");
+        });
+    });
+
+    describe("showcard(0)", function () {
+        it("should return 'sA'", function () {
+            expect(showcard(0)).toBe("sA");
+        });
+    });
+
+
+    describe("showcard(51)", function () {
+        it("should return 'dK'", function () {
+            expect(showcard(51)).toBe("dK");
+        });
+    });
+
+
+    describe("showsortedcard(0)", function () {
+        it("should return 'd8'", function () {
+            expect(showsortedcard(0)).toBe("d8");
+        });
+    });
+
+
+    describe("showsortedcard(51)", function () {
+        it("should return 'd2'", function () {
+            expect(showsortedcard(51)).toBe("d2");
+        });
+    });
+
+    describe("getplayerCard(1,1)", function () {
+        it("should return 's10'", function () {
+            expect(getplayerOneCard(1,1)).toBe("s10");
+        });
+    });
+
+    describe("getplayerCard(1,2)", function () {
+        it("should return 'h2'", function () {
+            expect(getplayerOneCard(1,2)).toBe("h2");
+        });
+    });
+    
+    describe("getplayerCard(2,1)", function () {
+        it("should return 's10'", function () {
+            expect(getplayerOneCard(2,1)).toBe("s10");
+        });
+    });
+
+    describe("getplayerCard(2,2)", function () {
+        it("should return 'h2'", function () {
+            expect(getplayerOneCard(2,2)).toBe("h2");
+        });
+    });
+    
+    describe("getplayerCard(3,1)", function () {
+        it("should return 's10'", function () {
+            expect(getplayerOneCard(3,1)).toBe("s10");
+        });
+    });
+
+    describe("getplayerCard(3,2)", function () {
+        it("should return 'h2'", function () {
+            expect(getplayerOneCard(3,2)).toBe("h2");
+        });
+    });
+    
+    describe("getplayerCard(4,1)", function () {
+        it("should return 's10'", function () {
+            expect(getplayerOneCard(4,1)).toBe("s10");
+        });
+    });
+
+    describe("getplayerCard(4,2)", function () {
+        it("should return 'h2'", function () {
+            expect(getplayerOneCard(4,2)).toBe("h2");
+        });
+    });
+    
+
 
 
 });
